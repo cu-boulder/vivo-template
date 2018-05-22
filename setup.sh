161f43eca809a5f2a5d25286cd126f4f71796faf
Don Elsborg <elsborg@colorado.edu>
2018-05-22 12:01:02 -0600

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
+git submodule init
+git submodule update
 cd Vivo
 git checkout maint-rel-1.9
 cd ../Vitro
-git checkout maint-rel.1.9
+git checkout maint-rel-1.9
 cd ../vivo-cuboulder
 git checkout v1.9.3
 
