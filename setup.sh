1eeca4e24346db53e7e9ee42e31e3885f8d0ca08
Elsborg Donald <elsborg@prometheus.int.colorado.edu>
2019-01-14 13:52:14 -0700

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@ git submodule init
 git submodule update
 cd Vivo
 git checkout maint-rel-1.9
+git pull origin maint-rel-1.9
 cd ../Vitro
 git checkout maint-rel-1.9
+git pull origin maint-rel-1.9
 cd ../vivo-cuboulder
-git checkout v1.9.3
+git checkout master
+git pull origin master
